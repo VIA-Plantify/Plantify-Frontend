b7621a581d4db4b52930a56190fbf7d61560216d
Rene <renealjou1.0@gmail.com>
2026-04-22 11:37:03 +0000

    Removing Node Server, and putting Axios directly to WebAPI

--- plantify-react-app/vite.config.ts
+++ plantify-react-app/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const VITE_PORT = 5173;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: VITE_PORT,
   }
 })
