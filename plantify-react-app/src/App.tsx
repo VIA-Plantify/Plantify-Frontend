4f3f4cb698a7a5ce758c8fdb3409338848049ce4
RodrigoM1502 <itzramyy@gmail.com>
2026-05-06 13:01:49 +0000

    Dark mode toggle for the 3 existing pages

--- plantify-react-app/src/App.tsx
+++ plantify-react-app/src/App.tsx
@@ -3,20 +3,22 @@ import Login from './Pages/Login.tsx';
 import Register from './Pages/Register.tsx';
 import PlantInfo from './Pages/PlantInfo.tsx';
 import './App.css'
+import { ThemeProvider } from './theme/ThemeContext';
 
 function App() {
 
 
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-            <Route path="/Register" element={<Register />}/>
-                <Route path="/PlantInfo" element={<PlantInfo/>}/>
-        </Routes>
-      </BrowserRouter>
-
-  )
+    return (
+        <ThemeProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/Register" element={<Register />}/>
+                    <Route path="/PlantInfo" element={<PlantInfo/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    )
 }
 
 export default App
